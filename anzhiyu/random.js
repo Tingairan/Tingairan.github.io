a2551fc73fd48196b55b1fd42cb71a96a4bbfae6
Tingairan <3040834799@qq.com>
2024-01-18 19:00:36 +0800

    Site updated: 2024-01-18 19:00:36

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/6d102583.html","posts/b9ff91c0.html","posts/c60b832a.html","posts/e33e3b9c.html","posts/2b9348ac.html","posts/c035d2d5.html","posts/3e128958.html"];function toRandomPost(){
+var posts=["posts/c60b832a.html","posts/e33e3b9c.html","posts/2b9348ac.html","posts/6d102583.html","posts/b9ff91c0.html","posts/3e128958.html","posts/c035d2d5.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
